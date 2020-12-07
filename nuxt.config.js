e36ad9b560872accb6bf4994ae2445e776400765
samuel.vallon <samuel.vallon@gmail.com>
2020-12-07 16:41:31 +0700

    add: photoswipe + change icons black, change background white

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,14 +10,24 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href:
+          'https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet"',
+        rel: 'stylesheet',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~/plugins/vue-stack-grid', mode: 'client' }],
+  plugins: [
+    { src: '~/plugins/vue-stack-grid', mode: 'client' },
+    { src: '~/plugins/v-photoswipe.js', ssr: false },
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
---
+++ plugins/v-photoswipe.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import { PhotoSwipe, PhotoSwipeGallery } from 'v-photoswipe'
+
+Vue.component('v-photoswipe', PhotoSwipe)
+Vue.component('v-photoswipe-gallery', PhotoSwipeGallery)
