1fb9af03d0a3f6c12108bdcf174dd8cedce86743
pywei088 <pywei088@gmail.com>
2020-12-08 16:00:38 +0700

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,12 @@
 <template>
-  <div
-    v-if="!loading"
-    class="px-4 w-full flex m-auto lg:px-10 lg:max-w-screen-lg xl:max-w-screen-xl"
-  >
-    <stack
-      :column-min-width="
-        windowWidth < 640
-          ? 130
-          : windowWidth < 768
-          ? 200
-          : windowWidth < 1024
-          ? 240
-          : 280
-      "
-      :gutter-width="windowWidth > 1024 ? 30 : 10"
-      :gutter-height="windowWidth > 1024 ? 30 : 10"
-      monitor-images-loaded
-    >
-      <stack-item
-        v-for="(post, i) in posts"
-        :key="i"
-        style="transition: transform 700ms"
-      >
-        <div
-          class="relative"
-          @mouseover="hovered = i"
-          @mouseleave="hovered = null"
-        >
-          <NuxtLink :to="post.slug">
-            <img v-if="!loading" :src="post.src" alt="" />
-          </NuxtLink>
-          <transition
-            enter-active-class="transition ease-in-out duration-200 transform"
-            enter-class="opacity-0 scale-70"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="transition ease-in-out duration-300 transform"
-            leave-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-70"
-          >
-            <button
-              @click="showPhotoSwipe(i)"
-              v-show="hovered === i"
-              class="absolute shadow bottom-0 right-0 focus:outline-none m-5 bg-white rounded-full z-30"
-            >
-              <ExpandSvg class="p-2" />
-            </button>
-          </transition>
-        </div>
-      </stack-item>
-    </stack>
-    <div>
-      <v-photoswipe
-        v-if="!loading"
-        :isOpen="isOpen"
-        :items="posts"
-        :options="options"
-        @close="hidePhotoSwipe"
-      ></v-photoswipe>
-    </div>
-  </div>
+  <imagesContainer :posts="posts" />
 </template>
 
 <script>
-import ExpandSvg from '../components/ExpandSvg.vue'
+import ImagesContainer from '../components/imagesContainer.vue'
 
 export default {
-  components: {
-    ExpandSvg,
-  },
+  components: { ImagesContainer },
 
   async asyncData({ $content }) {
     const posts = await $content('blog').fetch()
@@ -76,39 +15,6 @@ export default {
       posts,
     }
   },
-
-  data() {
-    return {
-      windowWidth: null,
-      loading: true,
-      hovered: false,
-      isOpen: false,
-      options: {
-        index: 0,
-      },
-      images: [],
-    }
-  },
-
-  mounted() {
-    this.loading = true
-    this.windowWidth = window.innerWidth
-    window.addEventListener('resize', () => {
-      this.windowWidth = window.innerWidth
-    })
-    Promise.all(this.posts.map((post) => this.getImageData(post))).then(
-      (res) => {
-        res.forEach((image) => {
-          this.posts.forEach((post) => {
-            if (post.src === image.src) {
-              this.posts[post] = Object.assign(post, image)
-            }
-          })
-        })
-        this.loading = false
-      }
-    )
-  },
   methods: {
     head() {
       return {
@@ -116,30 +22,6 @@ export default {
           { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
         ],
       }
-    },
-    findImg(post) {},
-    showPhotoSwipe(index) {
-      this.isOpen = true
-      this.$set(this.options, 'index', index)
-    },
-    hidePhotoSwipe() {
-      this.isOpen = false
-    },
-    async getImageData(post) {
-      return await new Promise((resolve) => {
-        const img = new Image()
-        img.onload = () => {
-          const w = img.width
-          const h = img.height
-
-          resolve({
-            h,
-            w,
-            src: post.src,
-          })
-        }
-        img.src = post.src
-      })
     },
   },
 }
